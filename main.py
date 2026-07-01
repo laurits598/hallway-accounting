64f0161aa01206236bde6ae66f4a1c0d7e3afebc
laurits598 <lau@LauBook.localdomain>
2026-07-02 00:21:23 +0200

    Some shit with sheet_handler.py which path i changed earlier. Part 2

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 from app.backend.accounting import get_resident_costs
 from app.backend.google_sheets import fetch_sheet_data
 from app.backend.server import generate_next_small_teddy_month
-from scripts import monthly_summary 
+from scripts import monthly_summary, sheet_handler
 import sqlite3
 import subprocess
 import textwrap
 import re
 import tempfile
 import os
-
-import sheet_handler
-
 
 '''
 Need an "end of the month" trigger to run a function/script automatically?
